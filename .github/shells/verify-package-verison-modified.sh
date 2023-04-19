391e0fba6d5994a020bf2a38413cc4d04d70a79b
tanqq <tanqingqing@qtarde.com.cn>
2023-04-19 19:09:55 +0800

    update

--- .github/shells/verify-package-verison-modified.sh
+++ .github/shells/verify-package-verison-modified.sh
@@ -7,21 +7,12 @@ LIGHT_CYAN='\033[1;36m'
 
 COMMIT_SHA=$1
 
-# if git diff --name-only HEAD~2 | grep -q -E "package.json"; then
-#     echo "The 'package.json' file has been modified in the last two commits"
-# else
-#     echo "The 'package.json' file has not been modified in the last two commits"
-# if git diff --name-only HEAD~2 | grep -q -E "package.json|changelog.md"; then
-#   echo "The 'package.json' or 'changelog.md' file has been modified in the last two commits"
-# else
-#   echo "The 'package.json' or 'changelog.md' file has not been modified in the last two commits"
-# fi
- git diff-tree --no-commit-id --name-only -r $COMMIT_SHA | grep -E "package.json" | while read -r line ; do
- git diff --name-only HEAD~2 | grep -E "package.json" | while read -r line ; do
-    echo -e "$LIGHT_CYAN Processing $line"
+git diff-tree --no-commit-id --name-only -r $COMMIT_SHA | grep -E "package.json" | while read -r line ; do
+git diff --name-only HEAD~2 | grep -E "package.json" | while read -r line ; do
+  echo -e "$LIGHT_CYAN Processing $line"
 
-    if ! git diff HEAD~2 $line | grep -q -E "\+\s+\"version\""; then
-      echo -e "${BROWN_ORANGE}version field no change"
-      continue
-    fi
+  if ! git diff HEAD~2 $line | grep -q -E "\+\s+\"version\""; then
+    echo -e "${BROWN_ORANGE}version field no change"
+    continue
+  fi
 done
